16973616b47fb29766804b4c004a717f6ae07074
Vuk Samardžić <samardzic.vuk@gmail.com>
2019-12-27 11:12:33 +0100

    Updates

---
+++ precache-manifest.a031e87dcc94e13ac20da531cc0a60e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f72e35619079d77077e393f69c814e07",
+    "url": "/vuksamardzic/devclub-todo/index.html"
+  },
+  {
+    "revision": "7351c39ea9cbfdd89e8a",
+    "url": "/vuksamardzic/devclub-todo/static/css/main.dea053eb.chunk.css"
+  },
+  {
+    "revision": "263617063d9e5c6f7955",
+    "url": "/vuksamardzic/devclub-todo/static/js/2.d60b283f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/vuksamardzic/devclub-todo/static/js/2.d60b283f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7351c39ea9cbfdd89e8a",
+    "url": "/vuksamardzic/devclub-todo/static/js/main.46e30677.chunk.js"
+  },
+  {
+    "revision": "9578d7bb188051043181",
+    "url": "/vuksamardzic/devclub-todo/static/js/runtime-main.d01bf59c.js"
+  }
+]);
